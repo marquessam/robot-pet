d43ede9a55ce9b7c8fc2aa2f8d3b62e83595a2c0
marquessam <samuelnmarques@gmail.com>
2025-01-08 19:33:48 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="min-h-screen w-full flex flex-col">
+        {/* A top-level container that could be used for global layout adjustments */}
+        <div className="flex-grow">
+          {children}
+        </div>
+      </body>
     </html>
   )
 }
