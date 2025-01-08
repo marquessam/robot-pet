9844756060a68e7e65879f97ab1370d8d27b85ac
marquessam <samuelnmarques@gmail.com>
2025-01-08 19:32:00 +0800

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,9 +6,21 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  safelist: [
+    'text-green-400',
+    'text-red-400',
+    // Add any additional dynamic classes here
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        neonGreen: '#4af626', // Custom color if needed
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    // Add Tailwind CSS plugins here if desired
+  ],
 }
+
 export default config
