c8bc0dd147a5b2dc28de02d1e1c20a452405b2af
marquessam <samuelnmarques@gmail.com>
2025-01-08 17:26:54 +0800

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import RobotPet from '@/components/RobotPet'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-8 flex items-center justify-center bg-gray-50">
+      <RobotPet />
+    </main>
+  )
+}
